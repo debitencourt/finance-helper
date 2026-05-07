27812388a42adb5f40ecf7a55d0a02a5513d974d
Lucas de Bitencourt Frasson <lucasb.frasson@gmail.com>
2026-05-06 21:22:33 -0300

    feat: initial header for layout testing

---
+++ src/components/header/header.js
@@ -0,0 +1,10 @@
+import './header.css'
+import { RxHamburgerMenu } from "react-icons/rx";
+
+export default function Header() {
+  return (
+    <div className="container">
+      {/* <RxHamburgerMenu className='hamburguer-menu' size={36} /> */}
+    </div>
+  );
+}
